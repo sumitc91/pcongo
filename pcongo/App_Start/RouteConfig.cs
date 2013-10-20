68256cdcaa1cdb6c604fffffab1ee89ab46e0eb5
sumitc91 <sumitchourasia91@gmail.com>
2013-10-20 11:56:00 +0530

    Started Project
    Started Project

---
+++ pcongo/App_Start/RouteConfig.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace pcongo
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute("Home",
+                "",
+                new { controller = "User", action = "Index" });
+
+            routes.MapRoute("Default",
+                "{id}/{controller}/{action}",
+                new { controller = "User", action = "Details" }
+
+                );
+        }
+    }
+}
---
+++ pcongo/Controllers/UserController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace pcongo.Controllers
+{
+    public class UserController : Controller
+    {
+        //
+        // GET: /User/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public virtual ActionResult Details(String id)
+        {
+
+            var db = new pcongoEntities();
+            var userDetails = db.users.SingleOrDefault(x => x.Username == id);
+            user user = userDetails;
+
+
+
+            return View("Details", user);
+        }
+
+    }
+}
